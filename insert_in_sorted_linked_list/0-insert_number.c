673b0d8a21176fbaba1343d0c193157b330ec305
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-09-06 14:35:39 +0200

    corrected insert node to include null list and insert at start

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -20,24 +20,25 @@ listint_t *insert_node(listint_t **head, int number)
 			return (NULL);
 		}
 
-	new_node->n = number;
+		new_node->n = number;
 
-	if (*head == NULL || (*head)->n >= number)
-	{
-		new_node->next = *head;
-		*head = new_node;
+		if (*head == NULL || (*head)->n >= number)
+		{
+			new_node->next = *head;
+			*head = new_node;
+			return (*head);
+		}
+
+		listint_t *current = *head;
+
+		while (current->next != NULL && current->next->n < number)
+		{
+			current = current->next;
+		}
+
+		new_node->next = current->next;
+		current->next = new_node;
+
+		return (*head);
+
 	}
-
-	listint_t *current = *head;
-
-	while (current->next != NULL && current->next->n < number)
-	{
-		current = current->next;
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (*head);
-
-}
