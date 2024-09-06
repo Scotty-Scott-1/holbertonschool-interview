3aff1e3b3c37a7c8a1734cf131d0e6e1e49df2fb
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-09-06 14:21:20 +0200

    added linked list algo

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert a node to a sorted list
+ * @head: pointer to head of list
+ * @number: number to insert
+ * Return: list
+ */
+
+
+listint_t *insert_node(listint_t **head, int number)
+
+	{
+		listint_t *new_node = malloc(sizeof(listint_t));
+
+		if (!new_node)
+		{
+			return (NULL);
+		}
+
+	new_node->n = number;
+
+	if (*head == NULL || (*head)->n >= number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+
+	listint_t *current = *head;
+
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (*head);
+
+}
