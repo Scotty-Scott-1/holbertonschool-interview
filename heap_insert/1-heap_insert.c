bf0abbbf602d44fdaeea9a23a7e5167a6be27d7b
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-09-15 00:21:35 +0200

    added task 1

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+heap_t *find_first_available(heap_t *root);
+void heapify_up(heap_t *node);
+
+/**
+ * heap_insert - insert a node to a binary tree
+ * @root: root of tree
+ * @value: value of the node
+ * Return: heap_t
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+
+	heap_t *new = malloc(sizeof(heap_t));
+
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	if (*root == NULL)
+	{
+		new->parent = NULL;
+		*root = new;
+		return (new);
+	}
+
+	heap_t *first_available = find_first_available(*root);
+
+	new->parent = first_available;
+	if (first_available->left == NULL)
+		first_available->left = new;
+	else
+		first_available->right = new;
+
+	heapify_up(new);
+	return (new);
+}
+
+/**
+ * find_first_available - insert a node to a binary tree
+ * @root: root of tree
+ * Return: heap_t
+ */
+
+heap_t *find_first_available(heap_t *root)
+{
+	if (root == NULL)
+	{
+		return (NULL);
+	}
+
+	if (root->left == NULL || root->right == NULL)
+	{
+		return (root);
+	}
+
+	if (root->parent != NULL)
+	{
+		if (root->parent->right->left == NULL || root->parent->right->right == NULL)
+		{
+			return (root->parent->right);
+		}
+	}
+
+	heap_t *left_slot = find_first_available(root->left);
+
+	if (left_slot->left == NULL || left_slot->right == NULL)
+	{
+		return (left_slot);
+	}
+
+	return (find_first_available(root->right));
+}
+
+/**
+ * heapify_up - if node if greater than parent value, swap
+ * @node: the new node
+ * Return: None
+ */
+
+void heapify_up(heap_t *node)
+{
+
+	while (node->parent != NULL && node->n > node->parent->n)
+	{
+		int temp = node->n;
+
+		node->n = node->parent->n;
+		node->parent->n = temp;
+	}
+}
--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -19,8 +19,10 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-void binary_tree_print(const binary_tree_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
+heap_t *heap_insert(heap_t **root, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 
 #endif /* BINARY_TREES_H */
