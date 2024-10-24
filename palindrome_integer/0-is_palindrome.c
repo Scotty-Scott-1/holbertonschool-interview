133402d96b72a4241ebf42159fbb6723c583598f
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-10-25 00:54:13 +0200

    added task 0

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks if n is a palindrome
+ *
+ * @n: number in question
+ *
+ * Return: int
+ */
+
+int is_palindrome(unsigned long n)
+{
+	unsigned int last = 0;
+	unsigned int reversed_n = 0;
+	unsigned int OG = n;
+
+	while (n > 0)
+	{
+		last = n % 10;
+		reversed_n = reversed_n * 10 + last;
+		n /= 10;
+	}
+
+	printf("OG number = %d\n", OG);
+	printf("New number = %d\n\n", reversed_n);
+
+	if (reversed_n == OG)
+		return (1);
+	else
+		return (0);
+}
---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,8 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+int main(int ac, char **av);
+int is_palindrome(unsigned long n);
+
+
+#endif /* PALINDROME_H */
