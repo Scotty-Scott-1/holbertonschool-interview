9e84dd6dfda65ba942242bc7cc3a9ef523924ea8
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-11-14 23:30:36 +0100

    added betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -4,38 +4,54 @@
 
 #include "menger.h"
 
+/**
+ * hole - check if the middle of the current 3x3 grid
+ *
+ * @row: current row
+ * @col: current col
+ *
+ * Return: int
+ */
 int hole(int row, int col)
 {
 	while (row > 0 || col > 0)
 	{
 		if (row % 3 == 1 && col % 3 == 1)
 		{
-			return 1;
+			return (1);
 		}
 		col /= 3;
-		row /=3;
+		row /= 3;
 	}
-	return 0;
+	return (0);
 }
 
-void menger(int level) {
 
-	if (level < 0) {
+/**
+ * menger - print a space if in middle of a cube or # if not
+ *
+ * @level: level of the cube
+ *
+ * Return: void
+ */
+void menger(int level)
+{
+
+	if (level < 0)
+	{
 		return;
 	}
 
-	int size = (int)pow(3,level);
+	int size = (int)pow(3, level);
 
 	for (int row = 0; row < size; row++)
 	{
 		for (int col = 0; col < size; col++)
 		{
 			if (hole(row, col))
-			{
 				printf(" ");
-			} else {
+			else
 				printf("#");
-			}
 		}
 		printf("\n");
 	}
