880ad7e6e049e387c681d53639ad217a894cf07b
Scotty_Scott-1 <swilliamfr@outlook.com>
2024-12-09 13:31:33 +0100

    corrected task 0

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,29 +4,28 @@ const request = require('request');
 const arg = process.argv[2];
 let array = [];
 
-request('https://swapi-api.hbtn.io/api/films/' + arg , (error, response, body) => {
-
-	if(error) {
-		console.log('error: ', error);
-	} else {
-		const data = JSON.parse(body);
-		const film_characters = data.characters;
-
-		const charpromise = film_characters.map(url => {
-			return new Promise((resolve, reject) => {
-				request(url, (error, response, body) => {
-					const data2 = JSON.parse(body);
-					resolve(data2.name);
-
-				});
-			});
-		});
-		Promise.all(charpromise).then(names => {array = names;
-			array.forEach(name => {
-				console.log(name);
-			})
-		});
-	}
-
+request('https://swapi-api.hbtn.io/api/films/' + arg, (error, response, body) => {
+  if (error) {
+    console.log('error: ', error);
+  } else {
+    const data = JSON.parse(body);
+    const filmCharacters = data.characters;
+    const charPromise = filmCharacters.map(url => {
+      return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+          if (error) {
+            console.log('error: ', error);
+          }
+          const data2 = JSON.parse(body);
+          resolve(data2.name);
+        });
+      });
+    });
+    Promise.all(charPromise).then(names => {
+      array = names;
+      array.forEach(name => {
+        console.log(name);
+      });
+    });
+  }
 });
-
