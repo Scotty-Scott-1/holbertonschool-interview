4670093e23d45dad7677963def83eacffddecdbd
Scotty_Scott-1 <swilliamfr@outlook.com>
2024-12-09 12:27:39 +0100

    added task 0

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const arg = process.argv[2];
+let array = [];
+
+console.log(arg);
+
+request("https://swapi-api.hbtn.io/api/films/" + arg , (error, response, body) => {
+
+	if(error) {
+		console.log("error: ", error);
+	} else {
+		const data = JSON.parse(body);
+		const film_characters = data.characters;
+		console.log(film_characters);
+
+		const charpromise = film_characters.map(url => {
+			return new Promise((resolve, reject) => {
+				request(url, (error, response, body) => {
+					const data2 = JSON.parse(body);
+					resolve(data2.name);
+
+				});
+			});
+		});
+		Promise.all(charpromise).then(names => {array = names;
+			console.log(array);
+		});
+	}
+
+});
+
