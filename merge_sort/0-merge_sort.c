4d804260ac8e717d2d9752627d3e0e4d9fa41ea4
Scotty-Scott-1 <swilliamfr@outlook.com>
2025-02-08 00:27:25 +0100

    corrected 0

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -1,71 +1,83 @@
 #include "sort.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+void merge_sort_recursive(int *array, int *tmp, size_t left, size_t right);
+void merge(int *array, int *tmp, size_t left, size_t mid, size_t right);
 
 /**
- * merge_sort - Sorts an array of integers
+ * merge_sort - sorts an array
  * @array: array to be sorted.
  * @size: size of array.
  */
 void merge_sort(int *array, size_t size)
 {
-    int *tmp;
+	int *tmp;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    tmp = malloc(sizeof(int) * size);
-    if (tmp == NULL)
-        return;
+	tmp = malloc(sizeof(int) * size);
+	if (tmp == NULL)
+		return;
 
-    merge_sort_recursive(array, tmp, 0, size - 1);
-    free(tmp);
+	merge_sort_recursive(array, tmp, 0, size - 1);
+	free(tmp);
 }
 
 /**
- * merge_sort_recursive - recursively splits and sorts the array.
- * @array: array to be sorted.
+ * merge_sort_recursive - recursively splits and sorts the array
+ * @array: array to be sorted
  * @tmp: temp array
- * @left: left index
- * @right: right index
+ * @left: left index of subarray
+ * @right: right index of subarray
  */
 void merge_sort_recursive(int *array, int *tmp, size_t left, size_t right)
 {
-    if (left >= right)
-        return;
+	if (left >= right)
+		return;
 
-    size_t mid = left + (right - left) / 2;
+	size_t mid = left + (right - left + 1) / 2 - 1;
 
-    merge_sort_recursive(array, tmp, left, mid);
-    merge_sort_recursive(array, tmp, mid + 1, right);
-    merge(array, tmp, left, mid, right);
+	merge_sort_recursive(array, tmp, left, mid);
+	merge_sort_recursive(array, tmp, mid + 1, right);
+	merge(array, tmp, left, mid, right);
 }
 
 /**
- * merge - merge two sorted arrays
+ * merge - merges two sorted subarrays
  * @array: array to be sorted.
  * @tmp: temp array
- * @left: left index
+ * @left: left index of first subarray
  * @mid: middle index
- * @right: right index
+ * @right: right index of second subarray
  */
 void merge(int *array, int *tmp, size_t left, size_t mid, size_t right)
 {
-    size_t i = left, j = mid + 1, k = left;
+	size_t i = left, j = mid + 1, k = left;
 
-    while (i <= mid && j <= right)
-    {
-        if (array[i] <= array[j])
-            tmp[k++] = array[i++];
-        else
-            tmp[k++] = array[j++];
-    }
+	printf("Merging...\n[left]: ");
+	print_array(array + left, mid - left + 1);
+	printf("[right]: ");
+	print_array(array + mid + 1, right - mid);
 
-    while (i <= mid)
-        tmp[k++] = array[i++];
+	while (i <= mid && j <= right)
+	{
+		if (array[i] <= array[j])
+			tmp[k++] = array[i++];
+		else
+			tmp[k++] = array[j++];
+	}
 
-    while (j <= right)
-        tmp[k++] = array[j++];
+	while (i <= mid)
+		tmp[k++] = array[i++];
 
-    for (i = left; i <= right; i++)
-        array[i] = tmp[i];
+	while (j <= right)
+		tmp[k++] = array[j++];
+
+	for (i = left; i <= right; i++)
+		array[i] = tmp[i];
+
+	printf("[Done]: ");
+	print_array(array + left, right - left + 1);
 }
