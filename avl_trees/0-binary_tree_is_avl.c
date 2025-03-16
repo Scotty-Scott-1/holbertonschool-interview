7b301595ee18ca7eeebb80218752b9af6fcb0237
Scotty-Scott-1 <swilliamfr@outlook.com>
2025-03-17 00:05:27 +0100

    avl

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -34,8 +34,7 @@ int is_bst(const binary_tree_t *tree, int min, int max)
 		return (1);
 	if (tree->n <= min || tree->n >= max)
 		return (0);
-	return (is_bst(tree->left, min, tree->n)
-	&& s_bst(tree->right, tree->n, max));
+	return (is_bst(tree->left, min, tree->n) && s_bst(tree->right, tree->n, max));
 }
 
 /**
